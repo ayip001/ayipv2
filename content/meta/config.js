84c1dfec12e0b1a24847f066984f034d4f1c0cf4
user <ayip002@gmail.com>
2018-11-21 15:57:13 +0800

    Update meta

--- content/meta/config.js
+++ content/meta/config.js
@@ -4,13 +4,13 @@ module.exports = {
   siteTitle: "Angus's Personal Site", // <title>
   shortSiteTitle: "Angus Yip", // <title> ending for posts and pages
   siteDescription: "It's my personal site.",
-  siteUrl: "https://ayip.io",
+  siteUrl: "https://angusyeet.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
   authorName: "Angus Yip",
-  authorTwitterAccount: "proofofangus",
+  authorTwitterAccount: "angusyeet",
   // info
   infoTitle: "Angus Yip",
   infoTitleNote: "🅱️ottom text",
@@ -22,13 +22,13 @@ module.exports = {
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "hi@ayip.io",
-  messengerLink: "https://web.facebook.com/messages/t/ayip001",
+  contactEmail: "hi@angusyeet.com",
+  messengerLink: "https://web.facebook.com/messages/t/angusyeet",
   // social
   authorSocialLinks: [
     { name: "github", url: "https://github.com/ayip001" },
-    { name: "instagram", url: "https://www.instagram.com/facade.photography" },
-    { name: "facebook", url: "http://facebook.com/ayip001" },
-    { name: "twitter", url: "https://twitter.com/proofofangus" },
+    { name: "instagram", url: "https://www.instagram.com/angusyeet" },
+    { name: "facebook", url: "http://facebook.com/angusyeet" },
+    { name: "twitter", url: "https://twitter.com/angusyeet" },
   ]
 };
